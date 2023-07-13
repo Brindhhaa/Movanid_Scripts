04442964f154de390a1c910553c7c1464259695b
Brindhhaa <brindha.srivatsav@gmail.com>
2023-07-13 14:36:55 -0700

    Added new feature

--- script_1.py
+++ script_1.py
@@ -1,89 +1,13 @@
-
-
-'''import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
-import tkinter as tk
-
-excel_file_path = "/Users/brindha/Downloads/movandiData.xlsx"
-
-
-
-def plotGraph(xName, yName,filterName):
-    df = pd.read_excel(excel_file_path, sheet_name="EVM", engine="openpyxl")
-
-    filter_range = range(30)
-    filtered_dfs = []
-    plot_data = []
-
-    for filter_value in filter_range:
-        filtered_df = df[df[filterName] == filter_value]
-        filtered_dfs.append(filtered_df)
-
-    plt.grid(True)  # Add gridlines
-
-    for filtered_df in filtered_dfs:
-        new_df = filtered_df[[xName, yName]]
-        xpoints = new_df[xName].values
-        ypoints = new_df[yName].values
-        plot_data.append((xpoints, ypoints))
-        plt.plot(xpoints, ypoints)
-        #plt.scatter(xpoints, ypoints, s=30)
-
-        x_ticks = np.linspace(min(xpoints), max(xpoints), num=30)
-        plt.grid()
-        plt.xlabel(xName)
-        plt.ylabel(yName)
-        plt.title("Graph of " + xName + " vs " + yName)
-        plt.xticks(rotation='vertical')
-        plt.ylim(0, 60)
-
-    plt.legend([f"{i}" for i in range(len(filtered_dfs))], loc='center left', bbox_to_anchor=(1, 0.5))
-
-    plt.show()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python script_1.py xName yName filterName")
-    else:
-        xName = sys.argv[1]
-        yName = sys.argv[2]
-        filterName = sys.argv[3]
-        plotGraph(xName, yName, filterName)'''
-
-'''if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-x", "--xName", help="Value for xName")
-    parser.add_argument("-y", "--yName", help="Value for yName")
-    parser.add_argument("-f", "--filterName", help="Value for filterName")
-    args = parser.parse_args()
-
-    xName = args.xName
-    yName = args.yName
-    filterName = args.filterName
-
-    plotGraph(xName, yName, filterName)'''
-
-        
-
-#x --> "Pout_casc[dBm]"
-#y --> Gain[dB]
-#filter --> MV2650B0_gain_idx
-
-#to run from command lines: python graph_plotter.py "Pin_casc[dBm]" "Gain[dB]" "MV2650B0_gain_idx"
-#can also use:
-
-#this is changed
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tkinter as tk
-from tkinter import OptionMenu, messagebox
+from tkinter import messagebox
 
+# Set the path to the Excel file
 excel_file_path = "/Users/brindha/Downloads/movandiData.xlsx"
 
+# Define the available options (column names) for selection
 options = [
     "Bench", "RF freq [Hz]", "Waveform name", "Ambient Temp [deg]", "chip temp [deg]",
     "SG_PowerLevel", "input_loss", "comp_cable_loss", "splitter_loss", "Pin_casc[dBm]",
@@ -99,81 +23,98 @@ def parse_filter_ranges(filter_ranges):
     parsed_ranges = []
     for filter_range in filter_ranges:
         if "-" in filter_range:
+            # Parse the filter range if it is a range of values
             start, end = map(int, filter_range.split("-"))
             parsed_ranges.append(list(range(start, end + 1)))
         else:
+            # Append the single filter value if it is not a range
             parsed_ranges.append([int(filter_range)])
     return parsed_ranges
 
 def plotGraph():
+    # Retrieve the selected x-axis, y-axis, filter indices, and filter ranges
     xName = x_var.get()
     yName = y_var.get()
-    filterNames = filter_var.get()
+    filterIndices = filter_listbox.curselection()
     filterRanges = parse_filter_ranges(filter_range_entry.get().split(","))
 
-    if xName and yName and filterNames and filterRanges:
-        if len(filterNames) != len(filterRanges):
+    # Check if all the required fields are selected
+    if xName and yName and filterIndices and filterRanges:
+        if len(filterIndices) != len(filterRanges):
+            # Display an error message if the number of filter names and ranges don't match
             messagebox.showerror("Error", "Number of filter names and ranges should match.")
             return
 
+        # Read the data from the Excel file
         df = pd.read_excel(excel_file_path, sheet_name="EVM", engine="openpyxl")
 
         filtered_dfs = []
         plot_data = []
+        legend_labels = []
 
-        for filterName, filterRange in zip(filterNames, filterRanges):
+        # Iterate over the selected filter indices and their corresponding filter ranges
+        for filterIndex, filterRange in zip(filterIndices, filterRanges):
             filter_dfs = []
+            filter_legend_labels = []  # Separate list for legend labels per filter range
             for filter_value in filterRange:
-                filtered_df = df[df[filterName] == filter_value]
+                # Filter the dataframe based on the selected filter index and value
+                filtered_df = df[df[options[filterIndex]] == filter_value]
                 filter_dfs.append(filtered_df)
+                filter_legend_labels.append(f"{options[filterIndex]} = {filter_value}")
 
             filtered_dfs.append(filter_dfs)
+            legend_labels.extend(filter_legend_labels)
 
-        plt.grid(True)  # Add gridlines
+        # Create a new figure for each plot
+        plt.figure()
 
-        for filter_dfs in filtered_dfs:
+        # Plot the data for each filtered dataframe
+        for filter_dfs, label in zip(filtered_dfs, legend_labels):
             for filtered_df in filter_dfs:
+                # Extract the selected x-axis and y-axis data
                 new_df = filtered_df[[xName, yName]]
                 xpoints = new_df[xName].values
                 ypoints = new_df[yName].values
                 plot_data.append((xpoints, ypoints))
-                plt.plot(xpoints, ypoints)
+                plt.plot(xpoints, ypoints, label=label)
+                plt.grid(True)  # Add gridlines
 
-                x_ticks = np.linspace(min(xpoints), max(xpoints), num=30)
-                plt.grid()
-                plt.xlabel(xName)
-                plt.ylabel(yName)
-                plt.title("Graph of " + xName + " vs " + yName)
-                plt.xticks(rotation='vertical')
-                plt.ylim(0, 60)
+        # Set the x-axis, y-axis labels, title, and formatting options
+        plt.xlabel(xName)
+        plt.ylabel(yName)
+        plt.title("Graph of " + xName + " vs " + yName)
+        plt.xticks(rotation='vertical')
+        plt.ylim(0, 60)
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-        plt.legend([f"Filter {i+1}" for i in range(len(filtered_dfs))], loc='center left', bbox_to_anchor=(1, 0.5))
-
+        # Display the plot
         plt.show()
     else:
+        # Display an error message if any of the required fields are not selected
         messagebox.showerror("Error", "Please select all fields.")
 
 # Create the GUI
 window = tk.Tk()
 
-# Create the dropdown menus
+# Create the dropdown menus for x-axis, y-axis, and filter options
 x_label = tk.Label(window, text="xName:")
 x_label.pack()
 x_var = tk.StringVar(window)
-x_dropdown = OptionMenu(window, x_var, *options)
+x_dropdown = tk.OptionMenu(window, x_var, *options)
 x_dropdown.pack()
 
 y_label = tk.Label(window, text="yName:")
 y_label.pack()
 y_var = tk.StringVar(window)
-y_dropdown = OptionMenu(window, y_var, *options)
+y_dropdown = tk.OptionMenu(window, y_var, *options)
 y_dropdown.pack()
 
 filter_label = tk.Label(window, text="filterName:")
 filter_label.pack()
-filter_var = tk.StringVar(window)
-filter_dropdown = OptionMenu(window, filter_var, *options, multiple=True)
-filter_dropdown.pack()
+filter_listbox = tk.Listbox(window, selectmode=tk.MULTIPLE)
+for option in options:
+    filter_listbox.insert(tk.END, option)
+filter_listbox.pack()
 
 filter_range_label = tk.Label(window, text="filterRanges (comma-separated):")
 filter_range_label.pack()
@@ -186,4 +127,3 @@ plot_button.pack()
 
 # Start the GUI event loop
 window.mainloop()
-
